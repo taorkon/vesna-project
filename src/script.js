4990f7803ddfbaf56f8bb97b161c18618f912124
taorkon <taorkon.tootta@gmail.com>
2023-04-01 20:01:00 +0300

    accordeaon for questions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
 // Our JS script
+//script for transition between question-icon & question-icon-active
+function initIntIcon(i) {
+    if (i.classList.contains("js-nc-int-icon-loaded")) return;
+    i.classList.add("js-nc-int-icon-loaded");
+    i.closest("svg").addEventListener("click", function (n) { i.classList.toggle("nc-int-icon-state-b") });
+};
+var intIcons = document.getElementsByClassName("js-nc-int-icon");
+for (var i = 0; intIcons.length > i; i++) { initIntIcon(intIcons[i]); }
